9fb7d44f51a9f4c4d4839ea9656f0bb42b03430c
ajaysharma12799 <ajaysharma12799@gmail.com>
2021-08-20 13:07:49 +0530

    react-hook-form-demo Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
+import {useForm} from "react-hook-form";
 import './App.css';
 
-function App() {
+const App = () => {
+  const [userInfo, setUserInfo] = useState();
+  const {register, handleSubmit, formState: {errors}} = useForm();
+  const onFormSubmit = (data) => {
+    setUserInfo(data);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <div className="Container">
+        <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+        <form className="form" onSubmit={handleSubmit(onFormSubmit)}>
+          <h1>Registration Form</h1>
+          <div className="form-Control">
+            <input type="text" placeholder="Username" name="username" {...register('username', {required: "Please Enter Username"})} />
+          </div>
+          <p className="error">{errors.username?.message}</p>
+          <div className="form-Control">
+            <input type="email"  placeholder="Email" name="email" {...register('email', {required: "Please Enter Email"})} />
+          </div>
+          <p className="error">{errors.email?.message}</p>
+          <div className="form-Control">
+            <input type="password"  placeholder="Password" name="password" {...register('password', {required: "Please Enter Password", minLength: {value: 4, message: "Password Should be More than 4 Characters"}, maxLength: {value: 10, message: "Password Should Not be More than 10 Characters"}})} />
+          </div>
+          <p className="error">{errors.password?.message}</p>
+          <div className="form-Control">
+            <input type="text" placeholder="PhoneNumber" name="phonenumber" {...register('phonenumber', {required: "Please Enter Phone Number", minLength: {value: 10, message: "Phone Number Should Not be Less than 10 Digits"}, maxLength: {value: 10, message: "Phone Number Should Not be More than 10 Digits"}})} />
+          </div>
+          <p className="error">{errors.phonenumber?.message}</p>
+          <div className="form-Control">
+            <input type="text" placeholder="Designation" name="designation" {...register('designation', {required: "Please Enter Designation"})} />
+          </div>
+          <p className="error">{errors.designation?.message}</p>
+          <div className="submit-button">
+            <button type="submit" className="btn">Validate</button>
+          </div>
+        </form>
+      </div>
+    </React.Fragment>
+  )
 }
 
-export default App;
+export default App
